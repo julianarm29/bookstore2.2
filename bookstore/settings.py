fef7410369f4b6251e3191a2f0a4ba2f690b2b52
Juliana Rodrigues <juliana.rodrigues-29@hotmail.com>
2023-11-30 11:39:26 -0300

    Feat: fix SECRET_KEY in settings.py

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-o)8jin1w1h$h)htev+)3_wdk59of9ysm8v5tm&7ek5av@(5ysw"
+SECRET_KEY = os.environ.setdefault("SECRET_KEY", "django-insecure-o)8jin1w1h$h)htev+)3_wdk59of9ysm8v5tm&7ek5av@(5ysw")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
