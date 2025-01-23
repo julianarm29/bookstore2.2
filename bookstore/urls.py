69c2c58b251a6ee4bb1d95153404824be337e7d0
Juliana Rodrigues <juliana.rodrigues-29@hotmail.com>
2025-01-23 20:50:18 +0100

    mudando urls views settings templates pythonanywhere

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -16,4 +16,6 @@ urlpatterns = [
     path('v1/products/', include('product.urls')),  # Prefixo 'v1/' para produtos
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('docs/', get_schema_view(title='Bookstore API'))
+    path("update_server/", views.update, name="update"),
+    path('hello/', views.hello_world, name='hello_world'),
 ]
